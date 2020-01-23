0d92440c428b0f4e2f600403b4b6c2514c01578e
mohit10696 <mohitsojitraa@gmail.com>
2020-01-23 12:34:07 +0530

    pratical8

---
+++ app/src/main/java/com/example/wcmc_praticals/fragment2.java
@@ -0,0 +1,16 @@
+package com.example.wcmc_praticals;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class fragment2 extends android.app.Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment2, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/wcmc_praticals/pratical8.java
+++ app/src/main/java/com/example/wcmc_praticals/pratical8.java
@@ -2,13 +2,33 @@ package com.example.wcmc_praticals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class pratical8 extends AppCompatActivity {
 
+    Fragment f1,f2,f3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pratical8);
+        setTitle("Pratical 8");
+        f1 = new fragment1();
+        f2 = new fragment2();
+        f3 = new fragment3();
+
+        loadFragment(f1,R.id.fragment1);
+        loadFragment(f2,R.id.fragment2);
+        loadFragment(f3,R.id.fragment3);
+
     }
+    private void loadFragment(Fragment fragment,int id) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(id, fragment);
+        fragmentTransaction.commit(); // save the changes
+    }
+
 }
