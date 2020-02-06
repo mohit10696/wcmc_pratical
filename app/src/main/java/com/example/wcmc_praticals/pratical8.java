9b7d14f37b97f558e116adb2804678ac7bb833ed
mohit10696 <mohitsojitraa@gmail.com>
2020-02-06 13:09:55 +0530

    pratical8

--- app/src/main/java/com/example/wcmc_praticals/pratical8.java
+++ app/src/main/java/com/example/wcmc_praticals/pratical8.java
@@ -6,12 +6,15 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class pratical8 extends AppCompatActivity {
+public class pratical8 extends AppCompatActivity implements View.OnClickListener {
 
     Fragment f1,f2,f3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button b1,b2,b3;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pratical8);
         setTitle("Pratical 8");
@@ -19,9 +22,15 @@ public class pratical8 extends AppCompatActivity {
         f2 = new fragment2();
         f3 = new fragment3();
 
-        loadFragment(f1,R.id.fragment1);
-        loadFragment(f2,R.id.fragment2);
-        loadFragment(f3,R.id.fragment3);
+//        loadFragment(f1,R.id.fragment1);
+//        loadFragment(f2,R.id.fragment2);
+//        loadFragment(f3,R.id.fragment3);
+        b1 = findViewById(R.id.f1);
+        b2 = findViewById(R.id.f2);
+        b3 = findViewById(R.id.f3);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
 
     }
     private void loadFragment(Fragment fragment,int id) {
@@ -31,4 +40,21 @@ public class pratical8 extends AppCompatActivity {
         fragmentTransaction.commit(); // save the changes
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.f1:{
+                loadFragment(f1,R.id.fragment1);
+                break;
+            }
+            case R.id.f2:{
+                loadFragment(f2,R.id.fragment1);
+                break;
+            }
+            case R.id.f3:{
+                loadFragment(f3,R.id.fragment1);
+                break;
+            }
+        }
+    }
 }
